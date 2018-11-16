9debed753b065cf58e2b087ff006b729b935aacf
James Cho <hwisoocho@gmail.com>
2018-11-16 11:11:34 -0800

    create array of words

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -2,7 +2,15 @@ namespace WordMaster
 {
   public class WordCounter
   {
-    public string UserInput = "cat";
+    public string UserInputWord = "cat";
+    public string UserInputSentence = "The cat house";
+
+    public bool CreateWordArray(string sentence)
+    {
+      string[] Arr = UserInputSentence.Split(" ");
+      string[] Arr2 = sentence.Split(" ");
+      return Arr.Length == Arr2.Length;
+    }
 
     public bool MatchesWord(string word)
     {
