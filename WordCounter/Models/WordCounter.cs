f2f847c1179685ec4a55fd1484f66219f6e73645
James Cho <hwisoocho@gmail.com>
2018-11-16 12:46:28 -0800

    check each word in foreach loop

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -4,7 +4,7 @@ namespace WordMaster
   {
     public string UserInputWord = "cat";
     public string UserInputSentence = "The cat house";
-
+    public int counter = 0;
     public bool CreateWordArray(string sentence)
     {
       string[] Arr = UserInputSentence.Split(' ');
@@ -12,7 +12,17 @@ namespace WordMaster
       return (Arr.Length == 3 && Arr2.Length ==3);
     }
 
-    
+    public bool CheckEachWord(string[] array)
+    {
+      foreach(string word in array)
+      {
+        if(MatchesWord(word) ==true)
+        {
+          counter++;
+        }
+      }  
+      return counter==1;
+    }
 
     public bool MatchesWord(string word)
     {
